dc00db439558f31aad5b7ccb2b45ace7a3ecc9a2
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-07-28 15:17:06 +0500

    коммит задания 1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-alert('hello world');
-console.log('hello world');
+const title = document.getElementById('title').innerHTML = 'title';
+const screens = document.getElementById('creens');
+const screenPrice = document.getElementById('screenPrice');
+const roolback = document.getElementById('roolback');
+const fullprice = document.getElementById('fullprice');
+const adaptive = document.getElementById('adaptive');
+
+
+alert('Js это просто');
+console.log('Js это просто');
+
+

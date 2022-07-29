8b093efaa17ea1c29e86b1630fcbbaa4077383ea
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-07-29 12:32:26 +0500

    сделал домашку

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let title = document.getElementById('title').innerHTML = 'title';
-let screens;
-let screenPrice;
-let roolback;
-let fullprice;
-let adaptive;
+let title = "lesson02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1234567890;
+let roolback = 99;
+let fullprice = 99999;
+let adaptive = false;
 
 
+console.log(typeof(title));
+console.log(typeof(fullprice));
+console.log(typeof(adaptive));
+console.log(screens.length);
 
-alert('Js это просто');
-console.log('Js это просто');
+console.log(screenPrice, 20507772.26, 75740361.35, 13641634.14);
+console.log(fullprice, 1661.11, 61349.08, 11049.61);
+console.log(screens.toLowerCase().split());
+console.log(fullprice * roolback / 100);

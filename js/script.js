f4c729ff46c0964ea89931546d0bb28684186404
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-09 20:13:25 +0500

    сделал домашку

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const title = document.getElementsByTagName('h1')[0]                                                           //
+console.log(title)
+//-------------------------------------------------------------------------
+
+const btnCalculate = document.getElementsByClassName('handler_btn')[0]
+console.log(btnCalculate)                                                                                   //
+
+const btnreset = document.getElementsByClassName('handler_btn')[1]
+console.log(btnreset)                                                                                       //
+//------------------------------------------------------------------------
+
+const btnScreen = document.querySelector('.screen-btn')
+console.log(btnScreen)
+//-----------------------------------------------------------------------
+
+const percent = document.querySelectorAll('.other-items.percent')
+console.log(percent)
+
+const number = document.querySelectorAll('.other-items.number ')
+console.log(number)
+//------------------------------------------------------------------------
+
+const input = document.querySelector('.rollback input[type="range"]')
+console.log(input)
+//-----------------------------------------------------------------------
+
+const span = document.querySelector('.rollback .range-value')
+console.log(span)
+//----------------------------------------------------------------------
+
+const totalInput = document.getElementsByClassName("total-input")
+console.log(totalInput)                                                                                     //
+//-----------------------------------------------------------------------
+
+let screen = document.querySelectorAll('.screen')
+console.log(screen)

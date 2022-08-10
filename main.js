6f17ab5f98f25fd7f6ceb32792da8220bc9d1343
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-11 01:47:28 +0500

    доделал

--- main.js
+++ main.js
@@ -19,11 +19,13 @@ books[0].children[2].querySelector('a').text = 'Книга 3. this и Прото
 let book2 = book[0].children[1]
 book2.children[3].after(book2.children[6])
 book2.children[4].after(book2.children[8])
+book2.children[9].after(book2.children[2])
 
 let book5 = book[5].children[1]
 book5.children[1].after(book5.children[9])
 book5.children[2].after(book5.children[4])
 book5.children[3].after(book5.children[5])
+book5.children[8].after(book5.children[6])
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-book[2].children[1].insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>')
+book[2].children[1].children[8].insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>')

04899db7909dec72e91894ac8404d5bb2a0c73e8
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-12 16:42:48 +0500

    сменил название переменных

--- js/script.js
+++ js/script.js
@@ -1,39 +1,26 @@
 const title = document.getElementsByTagName('h1')[0]                                                           //
-console.log(title)
 //-------------------------------------------------------------------------
 
-const btnCalculate = document.getElementsByClassName('handler_btn')[0]
-console.log(btnCalculate)                                                                                   //
-
-const btnreset = document.getElementsByClassName('handler_btn')[1]
-console.log(btnreset)                                                                                       //
+const startBtn = document.getElementsByClassName('handler_btn')[0]
+const resetBtn = document.getElementsByClassName('handler_btn')[1]
 //------------------------------------------------------------------------
 
-const btnScreen = document.querySelector('.screen-btn')
-console.log(btnScreen)
+const buttonPlus = document.querySelector('.screen-btn')
 //-----------------------------------------------------------------------
 
-const percent = document.querySelectorAll('.other-items.percent')
-console.log(percent)
-
-const number = document.querySelectorAll('.other-items.number ')
-console.log(number)
+const otherItemsPercent = document.querySelectorAll('.other-items.percent')
+const otherItemsNumber = document.querySelectorAll('.other-items.number ')
 //------------------------------------------------------------------------
 
-const input = document.querySelector('.rollback input[type="range"]')
-console.log(input)
-//-----------------------------------------------------------------------
-
-const span = document.querySelector('.rollback .range-value')
-console.log(span)
+const inputRange = document.querySelector('.rollback input[type="range"]')
+const inputRangeValue = document.querySelector('.rollback .range-value')
 //----------------------------------------------------------------------
 
-const layoutCost = document.getElementsByClassName("total-input")[0]
-const numberScreens = document.getElementsByClassName("total-input")[1]
-const additionalServices = document.getElementsByClassName("total-input")[2]
-const theTotalCost = document.getElementsByClassName("total-input")[3]
-const costIncludingRollback = document.getElementsByClassName("total-input")[4]
-console.log(layoutCost, numberScreens, additionalServices, theTotalCost, costIncludingRollback)
+const total = document.getElementsByClassName("total-input")[0]
+const totalCount = document.getElementsByClassName("total-input")[1]
+const totalCountOther = document.getElementsByClassName("total-input")[2]
+const fullTotalCount = document.getElementsByClassName("total-input")[3]
+const totalCountRollback = document.getElementsByClassName("total-input")[4]
 //-----------------------------------------------------------------------
 
 let screen = document.querySelectorAll('.screen')
